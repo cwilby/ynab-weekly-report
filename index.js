c052ef016128d2c2943c81074f0e9413d64fda7e
Cameron Wilby <cameron.wilby@titlexi.com>
2022-02-22 02:18:20 -0800

    Account for UTC timezone

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const cron = require('node-cron');
 const sendWeeklyReport = require('./commands/sendWeeklyReport');
 
-cron.schedule('0 10 * * 1', sendWeeklyReport);
+cron.schedule('0 18 * * 2', sendWeeklyReport);
 
 console.log('Cron jobs started');
